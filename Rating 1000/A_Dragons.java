972150b1e763eeeee46624385036780882e924f9
kshatriya-25 <ankitrajput0402@gmail.com>
2024-07-04 09:29:07 +0530

    LEARN TO SORT A PAIR CLASS 😉

---
+++ Rating 1000/A_Dragons.java
@@ -0,0 +1,200 @@
+/* OM VIGHNHARTAYE NAMO NAMAH : */
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class A_Dragons {
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                sb.append((char) c);
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    static Reader sc = new Reader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String args[]) throws IOException {
+        /*
+         * For integer input: int n=inputInt(); For long input: long n=inputLong(); For double
+         * input: double n=inputDouble(); For String input: String s=inputString(); Logic goes here
+         * For printing without space: print(a+""); where a is a variable of any datatype For
+         * printing with space: printSp(a+""); where a is a variable of any datatype For printing
+         * with new line: println(a+""); where a is a variable of any datatype
+         */
+        long s = inputLong();
+        int n = inputInt();
+        Pair pairs[] = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            pairs[i] = new Pair(inputInt(), inputInt());
+
+        }
+        Arrays.sort(pairs, Comparator.comparingInt(pair -> pair.dragon));
+        boolean defeat = false;
+        for (int i = 0; i < n; i++) {
+            if (s > pairs[i].dragon)
+                s += pairs[i].bonus;
+            else
+                defeat = true;
+        }
+        if (defeat)
+            println("NO");
+        else
+            println("YES");
+        out.flush();
+        out.close();
+    }
+
+    public static class Pair {
+        int dragon;
+        int bonus;
+
+        public Pair(int dragon, int bonus) {
+            this.dragon = dragon;
+            this.bonus = bonus;
+        }
+    }
+
+    public static int inputInt() throws IOException {
+        return sc.nextInt();
+    }
+
+    public static long inputLong() throws IOException {
+        return sc.nextLong();
+    }
+
+    public static double inputDouble() throws IOException {
+        return sc.nextDouble();
+    }
+
+    public static String inputString() throws IOException {
+        return sc.readLine();
+    }
+
+    public static <T> void print(T a) throws IOException {
+        out.print(a);
+        out.flush();
+    }
+
+
+    public static <T> void println(T a) throws IOException {
+        out.println(a);
+        out.flush();
+    }
+
+    public static <T> void println() throws IOException {
+        out.println();
+        out.flush();
+    }
+
+    public static int[] inputArray(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = inputInt();
+        }
+        return arr;
+    }
+
+    public static void printIntArray(int[] array) throws IOException {
+
+        for (int i : array) {
+            print(i + " ");
+        }
+        println();
+    }
+
+}
