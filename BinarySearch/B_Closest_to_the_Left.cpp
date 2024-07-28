e46f9cc40d2b5f5e6b2f0b1d1ae324d58e6efa14
kshatriya-25 <ankitrajput0402@gmail.com>
2024-07-28 11:46:30 +0530

    .

---
+++ BinarySearch/B_Closest_to_the_Left.cpp
@@ -0,0 +1,43 @@
+/* OM VIGHNHARTAYE NAMO NAMAH :*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define inf 10e10
+#define neginf -10e10
+#define int long long
+
+int handlequeries(vector<int> &arr, int target) {
+  int n = arr.size();
+  int l = -1;
+  int r = n;
+  while (l + 1 < r) {
+    int mid = l + (r - l) / 2;
+    if (arr[mid] <= target)
+      l = mid;
+    else
+      r = mid;
+  }
+  return r - 1;
+}
+
+signed main() {
+  ios ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int n, k;
+  cin >> n >> k;
+  vector<int> arr;
+  arr.push_back(neginf);
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    arr.push_back(x);
+  }
+  arr.push_back(inf);
+  while (k-- > 0) {
+    int q;
+    cin >> q;
+    cout << handlequeries(arr, q) << endl;
+  }
+}
